dae87f6a0cc0cb30ef1af7a3cd5367914ba06523
Molly Miller <55203910+mollym55@users.noreply.github.com>
2022-06-23 17:20:13 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -54,8 +54,7 @@ class server():
                 size = len(data)
                 con.send(self.default_headers(content_len=size).encode())
         except:
-            con.send(self.default_headers(status_code="404").encode() + b"<h1>Error 404: Not Found</h1>")
-            con.send("<h1>Error 404: Not Found</h1>").encode()
+            con.send((self.default_headers(status_code=404) + "<h1>Error 404: Not Found</h1>").encode())
 
 
     def get(self, path, con):
@@ -68,8 +67,7 @@ class server():
                 con.send(self.default_headers(content_len=size).encode())
                 con.send(data)
         except:
-            con.send(self.default_headers(status_code="404").encode() + b"<h1>Error 404: Not Found</h1>")
-            con.send("<h1>Error 404: Not Found</h1>").encode()
+            con.send((self.default_headers(status_code=404) + "<h1>Error 404: Not Found</h1>").encode())
             
     def error(self, con):
         error_response = self.default_headers(status_code="501")
