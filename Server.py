4d8f3722676941104913c35f78166abbc5d2c1c6
Molly Miller <55203910+mollym55@users.noreply.github.com>
2022-06-23 16:30:53 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -34,7 +34,7 @@ class server():
         else:
             self.error(con)
 
-    def default_headers(self, status_code=200, content_len=None):
+    def default_headers(self, status_code="200", content_len=None):
         headers = []
         headers.append("HTTP/1.1" + status_code + "OK\r\n")
         headers.append("Date: " + datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT') + "\r\n")
@@ -54,7 +54,7 @@ class server():
                 size = len(data)
                 con.send(self.default_headers(content_len=size).encode())
         except:
-            con.send((self.default_headers(status_code=404) + "<h1>Error 404: Not Found</h1>").encode())
+            con.send((self.default_headers(status_code="404") + "<h1>Error 404: Not Found</h1>").encode())
 
 
     def get(self, path, con):
@@ -67,10 +67,10 @@ class server():
                 con.send(self.default_headers(content_len=size).encode())
                 con.send(data)
         except:
-            con.send((self.default_headers(status_code=404) + "<h1>Error 404: Not Found</h1>").encode())
+            con.send((self.default_headers(status_code="404") + "<h1>Error 404: Not Found</h1>").encode())
 
     def error(self, con):
-        error_response = self.default_headers(status_code=501)
+        error_response = self.default_headers(status_code="501")
         error_response += "<h1>Error 501: Not Implemented</h1>"
         con.send(error_response.encode())
 
