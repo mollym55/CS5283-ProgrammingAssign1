4af745d656323be4865e3daaa810ef705976f44e
Molly Miller <55203910+mollym55@users.noreply.github.com>
2022-06-23 11:52:01 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -11,9 +11,44 @@ CRLF = "\r\n\r\n"
 # default method
 default = 'GET'  
 
+def configClientTCP(url):
+    socket.setdefaulttimeout(0.50)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.30)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    if 'localhost' in url:
+        path = getPath(url)
+
+        HOST = 'localhost'
+        if len(path) == 2:
+            # drop any trailing paths to file. For example 8080/index.html --> 8080
+            path = path[1].split('/')[0]
+            PORT = int(path) if path.isdigit() else 80
+
+    else:
+        # break apart url to get what we need
+        url = urlparse(url)
+        path = getPath(url.netloc)
+
+        HOST = path[0]  # The remote host
+        PORT = int(path[1]) if len(path) == 2 else 80  # grab port from path else default to 80
+
+    # connect to host
+    s.connect((HOST, PORT))
+    return s
+
+def getPath(url):
+    path = url.split(':')
+
+    if len(path) == 1 or len(path) == 2:
+        return path
+    else:
+        print("Invalid URL structure. You can only have at most one ':' in your URL.")
+        exit()  # get out!
+
 
 def GET(host, port):
-    s = Utility.configClientTCP(url)
+    s = configClientTCP(url)
     path = urlparse(url).path
     msg = "GET %s HTTP/1.0%s" % (path, CRLF)
     s.send(msg.encode())
