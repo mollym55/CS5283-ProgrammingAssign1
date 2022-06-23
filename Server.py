39458ca15c58d56f3e1ad0049587fa72e9f0b1a1
Molly Miller <55203910+mollym55@users.noreply.github.com>
2022-06-23 10:54:08 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,36 +1,88 @@
- next create a socket object
-s = socket.socket()        
-print ("Socket successfully created")
- 
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 12345               
- 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))        
-print ("socket binded to %s" %(port))
- 
-# put the socket into listening mode
-s.listen(5)    
-print ("socket is listening")  
+import socket
+import sys
+import datetime
+import locale
+locale.setlocale(locale.LC_TIME, 'en_US')
 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
- 
-# Establish connection with client.
-  c, addr = s.accept()    
-  print ('Got connection from', addr )
- 
-  # send a thank you message to the client. encoding to send byte type.
-  c.send('Thank you for connecting'.encode())
- 
-  # Close the connection with the client
-  c.close()
-   
-  # Breaking once connection closed
-  break
+
+class server():
+    def __init__(self, port, directory, host='localhost'):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((host, port))
+        sock.listen(1)
+        print(f"Listening on: {port}")
+        self.sock = sock
+
+    def serve(self):
+        while True:
+            con, address = self.sock.accept()
+            print(f"Connection from {address}")
+            request = con.recv(1024)
+            request = request.decode()
+            print(f"Request: {request}")
+            self.handle(request, con)
+            con.close()
+
+
+    def handle(self, request, con):
+        request = request.split(" ")
+        if request[0] == "GET":
+            self.get(request[1], con)
+        elif request[0] == "HEAD":
+            self.head(request[1], con)
+        else:
+            self.error(con)
+
+    def default_headers(self, status_code=200, content_len=None):
+        headers = f"HTTP/1.1 {status_code} OK\r\n"
+        headers += f"Date: {datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+        headers += "Server: Molly's Server\r\n"
+        headers += f"Content-Length: {content_len}\r\n"
+        headers += "Connection: close\r\n"
+        headers += "Content-Type: text/html; charset=UTF-8\r\n"
+        headers += "\r\n"
+        return headers
+
+    def head(self, path, con):
+        if path == "/":
+            path = "/index.html"
+        try:
+            with open(path[1:], "rb") as file:
+                data = file.read()
+                size = len(data)
+                con.send(self.default_headers(content_len=size).encode())
+        except FileNotFoundError:
+            con.send((self.default_headers(status_code=404) + "<h1>Error 404: Not Found</h1>").encode())
+
+
+    def get(self, path, con):
+        if path == "/":
+            path = "/index.html"
+        try:
+            with open(path[1:], "rb") as file:
+                data = file.read()
+                size = len(data)
+                con.send(self.default_headers(content_len=size).encode())
+                con.send(data)
+        except FileNotFoundError:
+            con.send((self.default_headers(status_code=404) + "<h1>Error 404: Not Found</h1>").encode())
+
+    def error(self, con):
+        error_response = self.default_headers(status_code=501)
+        error_response += "<h1>Error 501: Not Implemented</h1>"
+        con.send(error_response.encode())
+
+
+
+
+
+if __name__ == "__main__":
+    """python web_server.py PORT DIRECTORY"""
+    if len(sys.argv) != 3:
+        print("Usage: python web_server.py PORT DIRECTORY")
+        sys.exit(1)
+    else:
+        port = int(sys.argv[1])
+        directory = sys.argv[2]
+        server(port, directory).serve()
